581743d2dae43967438b934debc419796632933d
SylwiaKruszynska1 <221315718+SylwiaKruszynska1@users.noreply.github.com>
2026-05-26 10:12:06 +0200

    fix: fix code smells

--- src/app/app-entrypoint.component.ts
+++ src/app/app-entrypoint.component.ts
@@ -12,7 +12,7 @@ import { merge, mergeMap } from 'rxjs'
 export class AppEntrypointComponent implements OnInit {
   constructor(
     private translateService: TranslateService,
-    private config: PrimeNG
+    private readonly config: PrimeNG
   ) {}
 
   ngOnInit(): void {
