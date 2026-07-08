7aca150ebe0f476631d0b01e2954e577c85b31ad
Benjamin <166110368+BenjaminPabst@users.noreply.github.com>
2026-07-08 10:37:49 +0200

    feat: agent management (#39)
    * feat: add dashboard page
    
    * fix: linting
    
    * fix: remove comments
    
    * fix: standalone
    
    * fix: test case
    
    * feat: updated api, fixed tests and existing features
    
    * feat: improve test coverage for dashboard
    
    * fix: code smells
    
    * feat: improved test coverage
    
    * fix: improve test assertion
    
    * feat: add agent management
    
    * fix: sonar issues

---
+++ src/app/agent/pages/agent-details/agent-details.harness.ts
@@ -0,0 +1,9 @@
+import { ComponentHarness } from '@angular/cdk/testing'
+import { DataTableHarness, PageHeaderHarness } from '@onecx/angular-accelerator/testing'
+
+export class AgentDetailsHarness extends ComponentHarness {
+  static readonly hostSelector = 'app-agent-details'
+
+  getHeader = this.locatorFor(PageHeaderHarness)
+  getDataTable = this.locatorFor(DataTableHarness)
+}
